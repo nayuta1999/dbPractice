5dd26a399a19c094569a2fee878de62973559c2b
lrf141 <ghost141.kentyo@gmail.com>
2019-09-12 18:03:20 +0900

    色々変更

--- increase.sql
+++ increase.sql
@@ -7,4 +7,4 @@ insert into tweets (
     tweets.retweet, 
     tweets.include_reply,
     tweets.created_at
-    from tweets, tweets tweets2, tweets tweets3, tweets tweets4, tweets tweets5, tweets tweets6, tweets tweets7);
+    from tweets, tweets tweets2, tweets tweets3, tweets tweets4, tweets tweets5);
